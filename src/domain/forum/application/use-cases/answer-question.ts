a0e52f35ff76610eaa24aae880fd1f29c5b8fc8e
leonardo.mendes <leodsmf@gmail.com>
2024-12-08 12:31:03 -0300

    refatorando as pastas

---
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -0,0 +1,25 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { UniqueEntityID } from '../../../../core/entities/unique-entity-id'
+import { AnswersRepository } from '../../../forum/application/repositories/answers-repository'
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+export class AnswerQuestionUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
+
+    await this.answersRepository.create(answer)
+    return answer
+  }
+}
