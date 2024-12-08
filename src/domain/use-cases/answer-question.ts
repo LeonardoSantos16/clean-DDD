1e304c01dd8abdefac25661f57a70de7b4c6ba30
leonardo.mendes <leodsmf@gmail.com>
2024-12-08 00:18:18 -0300

    mapeando propriedades, getters e setters e abstração da criação de entidades

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { AnswersRepository } from "../repositories/answers-repository"
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -10,10 +11,10 @@ export class AnswerQuestionUseCase {
     private answersRepository: AnswersRepository,
   ) {}
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
         content,
-        authorId: instructorId,
-        questionId,
+        authorId: new UniqueEntityID(instructorId),
+        questionId: new UniqueEntityID(questionId),
       })
 
       await this.answersRepository.create(answer)
