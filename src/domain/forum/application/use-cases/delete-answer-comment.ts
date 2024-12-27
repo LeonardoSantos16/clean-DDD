26ff48608e0e2749f227ca01cbc6d2d569f985ac
leonardo <leodsmf@gmail.com>
2024-12-27 15:37:19 -0300

    Functional Error handling: definindo as classes

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,9 +1,10 @@
+import { Either, left, right } from '@/core/either'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 interface DeleteAnswerCommentUseCaseRequest {
   authorId: string
   answerCommentId: string
 }
-interface DeleteAnswerCommentUseCaseResponse {}
+type DeleteAnswerCommentUseCaseResponse = Either<string, {}>
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
   async execute({
@@ -14,12 +15,12 @@ export class DeleteAnswerCommentUseCase {
       answerCommentId,
     )
     if (!answerComment) {
-      throw new Error('Answer comment not found.')
+      return left('Answer comment not found.')
     }
     if (answerComment.authorId.toString() !== authorId) {
-      throw new Error('Not allowed')
+      return left('Not allowed')
     }
     await this.answerCommentsRepository.delete(answerComment)
-    return {}
+    return right({})
   }
 }
