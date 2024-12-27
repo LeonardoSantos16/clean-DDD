dc9fbcda9e416219919ca33bc18d97b492fc093a
leonardo.mendes <leodsmf@gmail.com>
2024-12-27 20:22:22 -0300

    Aggregates: criando pergunta com anexos

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,12 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Either, right } from '@/core/either'
-
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 interface CreateQuestionUseCaseRequest {
   authorId: string
   title: string
   content: string
+  attachmentsIds: string[]
 }
 
 type CreateQuestionUseCaseResponse = Either<
@@ -30,6 +31,13 @@ export class CreateQuestionUseCase {
       content,
     })
 
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      })
+    })
+    question.attachments = questionAttachments
     await this.questionsRepository.create(question)
 
     return right({
