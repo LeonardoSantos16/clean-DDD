1cb92403c98d809c0d3ccda4dc47a34fae25df70
leonardo.mendes <leodsmf@gmail.com>
2024-12-27 23:30:42 -0300

    Subdomínios & Domain Events: Ouvindo um evento de domínio

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -2,6 +2,7 @@ import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { DomainEvents } from '@/core/events/domain-events'
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
 
@@ -34,6 +35,7 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer) {
     this.items.push(answer)
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async delete(answer: Answer) {
@@ -45,5 +47,6 @@ export class InMemoryAnswersRepository implements AnswersRepository {
   async save(answer: Answer) {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
     this.items[itemIndex] = answer
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 }
